e23e48119c541a1ee523f8c8f462a55966daab0a
CelGui <jaycelricguillaume@gmail.com>
2020-03-13 20:08:28 +0400

    Une Fonction add()

---
+++ addTwoInt.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import sys
+
+def add(x ,y):
+    x = ( sys.argv[1] )
+    y = ( sys.argv[2] )
+
+if __name__==__"main":
+    print(sys.argv)
+    print( int(sys.argv[1]) + int(sys.argv[2]))
