fcf8b7e82b0a5b2cdf56e1dc2ec74807794cb227
CelGui <jaycelricguillaume@gmail.com>
2020-03-13 20:34:58 +0400

    Un message d'erreur s'affiche

--- addTwoInt.py
+++ addTwoInt.py
@@ -1,11 +1,21 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 import sys
 
-def add(x ,y):
-    x = ( sys.argv[1] )
-    y = ( sys.argv[2] )
+def add():
+    x = int(sys.argv[1])
+    y = int(sys.argv[2])
 
-if __name__==__"main":
-    print(sys.argv)
-    print( int(sys.argv[1]) + int(sys.argv[2]))
+if __name__== "__main__":
+        if (len(sys.argv) < 3):
+                print("Erreur!")
+                print("Le nombre de valeurs inserer doit etre deux.")
+
+        elif (len(sys.argv) > 3):
+                print("Erreur!")
+                print("Inserer pas plus que de deux valeurs.")
+
+        else:
+                print(sys.argv)
+                print("La somme est: ") + (str(int(sys.argv[1]) + int(sys.argv[2])))
+
