740ad23571845396292727138bbdcca7b2e53c9d
master <jovanylisette20@gmail.com>
2020-03-13 14:03:01 -0400

    Une fonction mul

---
+++ mulTwoInt.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import sys
+
+def mul(x ,y):
+     x = ( sys.argv[1] )
+     y = ( sys.argv[2] )
+
+if __name__==__"main":
+ 
+    print(sys.argv)
+    print( int(sys.argv[1]) * int(sys.argv[2]))
