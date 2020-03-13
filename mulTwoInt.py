b2e25659e7010b109d78bac9c9ded9022c33a5d9
master <jovanylisette20@gmail.com>
2020-03-13 14:36:36 -0400

    Demande d'une valeur

--- mulTwoInt.py
+++ mulTwoInt.py
@@ -1,20 +1,13 @@
-#1/usr/bin/env python
-
 import sys
 
 def mul():
-    x = int(sys.argv[1])
-    y = int sys.argv[2])
+        return x*y
 
 if __name__=="__main__":
-        if (len(sys.argv) ,3):
-                print("Erreur!")
-                print("Le nombre de valeurs a inserer est de 2.")
 
-        elif (len(sys.argv) > 3):
-                print("Erreur!")
-                print("Inserez pas plus que deux valeurs.")
-
-        else:
-                print(sys.argv)
-                print("La somme est:") * (str(int(sys.argv[1]) * int(sys.argv[2])))
+          if ( len(sys.argv) == 1 ):
+                    x = input( " La premiere valeur est, svp: ")
+                    y = input( " La deuxieme valeur est, svp: ")
+                    x = int(x)
+                    y = int(y)
+                    print("La somme est: ", mul() )
