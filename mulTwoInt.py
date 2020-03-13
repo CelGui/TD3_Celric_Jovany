30547c9824b57edad2ee4b668c41c3c0b4f11619
master <jovanylisette20@gmail.com>
2020-03-13 14:19:31 -0400

    Une message d'erreur

--- mulTwoInt.py
+++ mulTwoInt.py
@@ -1,12 +1,20 @@
-#!/usr/bin/env python3
+#1/usr/bin/env python
 
 import sys
 
-def mul(x ,y):
-     x = ( sys.argv[1] )
-     y = ( sys.argv[2] )
+def mul():
+    x = int(sys.argv[1])
+    y = int sys.argv[2])
 
-if __name__==__"main":
- 
-    print(sys.argv)
-    print( int(sys.argv[1]) * int(sys.argv[2]))
+if __name__=="__main__":
+        if (len(sys.argv) ,3):
+                print("Erreur!")
+                print("Le nombre de valeurs a inserer est de 2.")
+
+        elif (len(sys.argv) > 3):
+                print("Erreur!")
+                print("Inserez pas plus que deux valeurs.")
+
+        else:
+                print(sys.argv)
+                print("La somme est:") * (str(int(sys.argv[1]) * int(sys.argv[2])))
