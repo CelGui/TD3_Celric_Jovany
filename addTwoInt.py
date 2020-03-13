542e339935fa11cb8fb4b434445ff29feaa5ca7b
CelGui <jaycelricguillaume@gmail.com>
2020-03-13 21:01:47 +0400

    Demande de valeurs

--- addTwoInt.py
+++ addTwoInt.py
@@ -1,21 +1,14 @@
-#!/usr/bin/env python
-
 import sys
 
 def add():
-    x = int(sys.argv[1])
-    y = int(sys.argv[2])
+        return x+y
 
 if __name__== "__main__":
-        if (len(sys.argv) < 3):
-                print("Erreur!")
-                print("Le nombre de valeurs inserer doit etre deux.")
-
-        elif (len(sys.argv) > 3):
-                print("Erreur!")
-                print("Inserer pas plus que de deux valeurs.")
-
-        else:
-                print(sys.argv)
-                print("La somme est: ") + (str(int(sys.argv[1]) + int(sys.argv[2])))
+ 
+        if ( len(sys.argv) == 1 ):
+                 x = input( " La premiere valeur, svp: ")
+                 y = input( " La deuxieme valeur, svp: ")
+                 x = int(x)
+                 y = int(y)
+                 print("La somme est: ", add() )
 
